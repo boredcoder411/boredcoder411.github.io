7d58bd833f508d6600a7fbe7713936d31a725984
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2022-11-18 11:47:44 +0000

    updated cmd logging

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ var i = 0;
 var txt = "Welcome to boredcoder411's website! To get started, type help";
 var speed = 50;
 
+var li = document.createElement('li');
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typing").innerHTML += txt.charAt(i);
@@ -21,5 +23,7 @@ input.addEventListener("keypress", function(event) {
     event.preventDefault();
     cmd = document.getElementById("cmdline").value;
     document.getElementById("cmdline").value = "";
+    li.innerHTML = ` > ${cmd}<br>`;
+    document.getElementsByTagName("ul").appendChild(li);
   }
 });
