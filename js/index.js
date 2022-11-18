9f36da01751b962ee94b5823920e03b81aeb4742
boredcoder411 <>
2022-11-18 15:40:00 +0000

    added more commands

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var i = 0;
 var txt = "Welcome to boredcoder411's website! To get started, type help";
 var speed = 50;
 
-var li = document.createElement('li');
+var li;
 
 function typeWriter() {
   if (i < txt.length) {
@@ -10,6 +10,13 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
+}
+
+function addli(text) {
+  li = document.createElement('li')
+  li.innerHTML = text;
+  document.querySelector("ul").appendChild(li);
+  cmd = "";
 }
 
 typeWriter();
@@ -23,12 +30,31 @@ input.addEventListener("keypress", function(event) {
     event.preventDefault();
     cmd = document.getElementById("cmdline").value;
     document.getElementById("cmdline").value = "";
-    li = document.createElement('li')
-    li.innerHTML = ` > ${cmd}<br>`;
-    document.querySelector("ul").appendChild(li);
     switch (cmd) {
+      case "github":
+        addli(` > ${cmd}`);
+        addli(`Redirected to my github!`);
+        window.open("https://github.com/boredcoder411/", '_blank');
+        break;
+      case "langs":
+        addli(` > ${cmd}`);
+        addli("List of languages I know and how good I am at them:<br> Javascript (5/5)<br> Rust (4/5)<br> Batch (4/5)<br> Bash (5/5)<br> Python (3/5)<br> Java (3/5)<br> Scratch (5/5)<br> R (1/5)<br> PHP (3/5)<br> HTML (5/5)<br> CSS (4/5)<br> SQL (4/5)<br> zsh (5/5)<br> NIX (5/5)");
+        break;
+      case "help":
+        addli(` > ${cmd}`);
+        addli("Type help to get started.");
+        break;
       case "clear":
         document.querySelector("ul").innerHTML = "";
+        break;
+      default:
+        if (cmd == "") {
+          addli(` > ${cmd}`);
+        } else {
+          addli(` > ${cmd}`);
+          addli(` > Command "${cmd}" not recognized. Type help to get started.`);
+        }
+        break;
     }
   }
 });
